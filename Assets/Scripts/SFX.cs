d4eb6a68865851a94ba25e033ad1211f3143fd35
Crafteurmax <74319465+Crafteurmax@users.noreply.github.com>
2023-12-23 18:53:56 +0100

    fix sound duplication

--- Assets/Scripts/SFX.cs
+++ Assets/Scripts/SFX.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class SFX : MonoBehaviour
 {
     // Start is called before the first frame update
+    static bool exist = false;
     void Start()
     {
+        if (SFX.exist) Destroy(this.gameObject);
         DontDestroyOnLoad(this.gameObject);
+        SFX.exist = true;
     }
 
     // Update is called once per frame
